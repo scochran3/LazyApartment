75aa0266bcde7aad64e2d28b5749f200fe1d349b
Shawn Cochran <scochran3@gmail.com>
2019-07-20 12:47:04 -0400

    Updated EB settings

--- lazynyrenter/lazynyrenter/settings.py
+++ lazynyrenter/lazynyrenter/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'lazynyrenter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+if 'aa10uxjrfmskobd' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
